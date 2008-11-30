4bc0329fc86e95f074f8d2b120fd273ea93c05ca
Benny Wong <bdotdub@gmail.com>
2008-11-29 22:54:21 -0500

    oy, ive done a bunch, mostly layout and graphic stuff. im about to make the change from syndication to  nokogiri

--- lib/podcast.rb
+++ lib/podcast.rb
@@ -3,11 +3,43 @@ require 'open-uri'
 require 'syndication/rss'
 require 'syndication/podcast'
 
-parser = Syndication::RSS::Parser.new
-feed = parser.parse open('http://feeds.feedburner.com/tacksharp').read
+require 'xspf'
 
-require 'pp'
+module Snowpea
+  class Podcast
+    def initialize(args)
+      if not args[:url]
+        raise ArgumentError.new('Podcast URL not provided')
+      end
 
-pp feed
-
-
+      @parser = Syndication::RSS::Parser.new
+      @feed = @parser.parse open(args[:url])
+    end
+    
+    def to_xspf
+      @trackList = XSPF::Tracklist.new
+      @feed.items.each do |item|
+        @trackList << XSPF::Track.new(
+          :location   => item.enclosure.url,
+          :identifier => item.guid,
+          :title      => item.title,
+          :image      => @feed.channel.itunes_image['href']
+        )
+      end
+      
+      playlist = XSPF::Playlist.new( {
+         :xmlns => 'http://xspf.org/ns/0/',
+         :title => 'Tout est calme',
+         :creator => 'Yann Tiersen',
+         :license => 'Redistribution or sharing not allowed',
+         :info => 'http://www.yanntiersen.com/',
+         :tracklist => @trackList,
+         :meta_rel => 'http://www.example.org/key',
+         :meta_content => 'value',
+       })
+       xspf = XSPF.new( { :playlist => playlist } )
+       
+       return xspf
+    end
+  end
+end
--- snowpea.rb
+++ snowpea.rb
@@ -1,6 +1,9 @@
 require 'rubygems'
 require 'sinatra'
 
+require 'lib/podcast'
+require 'xspf'
+
 get '/' do
   haml :index
 end
@@ -8,8 +11,37 @@ end
 get '/listen' do
   @url = params[:url]
   if @url
-    haml '= @url'
+    @playlist = '/xspf/playlist.xml'
+    haml :listen
   else
+    @notice = 'Please enter a Feed'
+    haml :index
   end
 end
 
+get '/xspf' do
+  @url = params[:url]
+  @xspf = ''
+  
+  podcast = Snowpea::Podcast.new(:url => @url)
+  time2 = Time.now.to_i
+  
+  @xspf = podcast.to_xspf().to_xml
+  
+  f = File.open('public/xspf/playlist.xml', 'w')
+  f.write(@xspf)
+  f.close
+  
+  haml :index
+end
+
+get '/stylesheets/snowpea.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass :snowpea
+end
+
+helpers do
+  def parse_and_cache
+    
+  end
+end
